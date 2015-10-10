4dc660ec596b40ba810aad8a25d6b37b8145d234
Tsukalos <pedrolamkowski@gmail.com>
2015-10-09 23:24:33 -0300

    Changes

--- themesaver.py
+++ themesaver.py
@@ -17,11 +17,6 @@ import os
 import os.path
 import logging
 
-
-logging.basicConfig(filename='log.log', level=logging.DEBUG)
-logging.debug('Log start')
-logging.debug('/////////')
-
 def downloadhook(count, blockSize, totalSize):
     percent = int(count*blockSize*100/totalSize)
     sys.stdout.write("Download progress: %d%%   \r" % (percent) )
@@ -32,12 +27,17 @@ if not os.path.exists("files"):
     print("Creating 'files' folder...")
     os.makedirs("files")
     print("... Done")
+	
 #defines location to save file
 downloadlocation = "files/"
+
 r = praw.Reddit('WebmAnimeSaver')
+
 print("Hello, welcome to the /r/AnimeThemes submission downloader, ")
 print("please enter the interval time (minutes) between the searches: ")
 #^
+
+
 while True:
     try:
         waittime = int(input())
@@ -47,6 +47,8 @@ while True:
 print("Enter the number of new submissions,")
 print("the program should check each time (MAX 100): ")
 #^
+
+
 while True:
     try:
         postnumber = int(input())
@@ -56,20 +58,32 @@ while True:
             break
     except:
         print("There was a error in the value, please input a valid number!")
+		
+		
 subreddit = r.get_subreddit('animethemes')
+
+
 while True:
     print("Getting submission information (may take time depending of new submissions number)...")
+	
     for submission in subreddit.get_new(limit=postnumber):
         fileurl = submission.url
+		
         postdomain = submission.domain
+		
         filetitle = submission.title.replace('"', '').replace(':', '').replace('?', '').replace('/','').replace("'","")
+		
         print("")
         print("")
         print("")
         print("DOWNLOADING FROM: [" + fileurl + "]")
         print("FILE TITLE: [" + filetitle + "]")
         print("LOCATION: [" + downloadlocation + "]")
+		
+		
         fileloc = downloadlocation + filetitle + ".webm"
+		
+		
         if os.path.isfile(fileloc) == False and postdomain != 'self.AnimeThemes':
             urllib.request.urlretrieve(fileurl,fileloc, reporthook=downloadhook)
             logging.info(fileurl)
@@ -79,9 +93,13 @@ while True:
         else:
             print("File already present, jumping to next...")
             time.sleep(2)
+			
+			
         print("///////////////////////////////////////")
     print("")
+	
     print("> > > > No more new submissions, entering rest...")
+	
     for x in range(0,waittime):
         timeleft = ((waittime*60)-(60*x))/60
         print(str(int(timeleft)) + " mintues remaining to next check...") 
