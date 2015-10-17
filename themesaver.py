0388364093615f033db66ac1af41f0c12d4fda60
Tsukalos <pedrolamkowski@gmail.com>
2015-10-17 17:41:26 -0300

    Added
    no-unicode for namimg files
    logging ulrlib errors

--- themesaver.py
+++ themesaver.py
@@ -12,10 +12,14 @@
 import praw
 import sys
 import urllib.request
+import urllib.error
 import time
 import os
 import os.path
 import logging
+
+logging.basicConfig(filename = "loggerEvents.log")
+
 
 def downloadhook(count, blockSize, totalSize):
     percent = int(count*blockSize*100/totalSize)
@@ -52,7 +56,7 @@ print("the program should check each time (MAX 100): ")
 while True:
     try:
         postnumber = int(input())
-        if postnumber > 100:
+        if postnumber > 200:
             print("The value is too high, please input a value lower than 100 submissions")
         else:
             break
@@ -64,45 +68,59 @@ subreddit = r.get_subreddit('animethemes')
 
 
 while True:
-    print("Getting submission information (may take time depending of new submissions number)...")
-	
-    for submission in subreddit.get_new(limit=postnumber):
-        fileurl = submission.url
+	print("Getting submission information (may take time depending of new submissions number)...")
+
+	for submission in subreddit.get_new(limit=postnumber):
+		fileurl = submission.url
+		postdomain = submission.domain
+		title = bytes(submission.title.replace('"', '').replace(':', '').replace('?', '').replace('/','').replace("'","").encode('ascii','ignore'))
+		filetitle = title.decode('unicode_escape')
+		try:
+			url = urllib.request.urlopen(fileurl)
+		except (urllib.error.URLError, urllib.error.HTTPError) as err:
+			print("///////////////////////////////////////")
+			print("[There was a error with requesting the file from the url:] ")
+			print(fileurl)
+			print(filetitle)
+			print("[Error message:]" , err.reason, err.code)
+			logging.error("Error in requesting %s from url %s",filetitle,fileurl)
+			logging.error("%s %s",err.reason, err.code)
+			logging.error(err.headers)
+			print("[Error details in loggerEvents.log]")
+			continue
 		
-        postdomain = submission.domain
 		
-        filetitle = submission.title.replace('"', '').replace(':', '').replace('?', '').replace('/','').replace("'","").replace("\u2665","")
-		
-        print("")
-        print("")
-        print("")
-        print("DOWNLOADING FROM: [" + fileurl + "]")
-        print("FILE TITLE: [" + filetitle + "]")
-        print("LOCATION: [" + downloadlocation + "]")
+		print("")
+		print("")
+		print("")
+		print("[DOWNLOADING FROM:] " + fileurl )
+		print("[FILE TITLE:] " + filetitle)
+		print("[LOCATION:] " + downloadlocation)
 		
 		
-        fileloc = downloadlocation + filetitle + ".webm"
+		fileloc = downloadlocation + filetitle + ".webm"
 		
 		
-        if os.path.isfile(fileloc) == False and postdomain != 'self.AnimeThemes':
-            urllib.request.urlretrieve(fileurl,fileloc, reporthook=downloadhook)
-            logging.info(fileurl)
-            logging.info(fileloc)
-            logging.info('')
-            print("DOWNLOAD COMPLETE.")
-        else:
-            print("File already present, jumping to next...")
-            time.sleep(2)
+		if os.path.isfile(fileloc) == False and postdomain != 'self.AnimeThemes':
+			urllib.request.urlretrieve(fileurl,fileloc, reporthook=downloadhook)
+			logging.info(fileurl)
+			logging.info(fileloc)
+			logging.info('')
+			print("[DOWNLOAD COMPLETE.]")
+		else:
+			print("[File already present, jumping to next...]")
+			time.sleep(2)
 			
 			
-        print("///////////////////////////////////////")
-    print("")
+		print("///////////////////////////////////////")
+		
+	print("")
 	
-    print("> > > > No more new submissions, entering rest...")
+	print("[> > > > No more new submissions, entering rest...]")
 	
-    for x in range(0,waittime):
-        timeleft = ((waittime*60)-(60*x))/60
-        sys.stdout.write(str(int(timeleft)) + " mintues remaining to next check...  \r")
-        sys.stdout.flush()
-        time.sleep(60)
+	for x in range(0,waittime):
+		timeleft = ((waittime*60)-(60*x))/60
+		sys.stdout.write("["+str(int(timeleft)) + " mintues remaining to next check...  ]\r")
+		sys.stdout.flush()
+		time.sleep(60)
 
